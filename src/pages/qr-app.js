733700143827616a9b903585f3945a41b2773758
emmusic <emklassenmusic@gmail.com>
2022-08-16 11:26:38 -0400

    adding global window to qr code

--- src/pages/qr-app.js
+++ src/pages/qr-app.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import QRScan from 'qrscan'
 import { Fragment } from 'react'
+import { QrCodeRounded } from '@mui/icons-material'
 
+
+const { requestAnimationFrame } = global
 class QRApp extends Component {
 
   constructor (props) {
@@ -13,18 +16,31 @@ class QRApp extends Component {
   onFind (value) {
     this.setState({ value, watching: false })
   }
+  
 
   render () {
+    let message
+    if (this.state.notEnabled) {
+      message = <div><span role='img' aria-label='camera'>🎥</span> Unable to access video stream (please make sure you have a webcam enabled)</div>
+    } else if (this.state.loading) {
+      message = <div><span role='img' aria-label='time'>⌛</span> Loading video...</div>
+    }
+
     return (
       <Fragment>
-        <h2>Check In Now</h2>
+        <h2>Push the QR code to scan with your phone</h2>
         {this.state.watching
           ? (
             <QRScan onFind={this.onFind} />
           )
           : (
             <Fragment>
-              <button onClick={() => this.setState({ watching: true })}>Scan</button>
+              <QrCodeRounded onClick={() => this.setState({ watching: true })}>
+
+              </QrCodeRounded>
+              <div id="qr-reader"></div>
+              <meta charset="UTF-8" />
+              <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
               <h4>value: {this.state.value}</h4>
             </Fragment>
           )
