250247ba7b5492ecc88099bc294ea6519a6b9daa
chloepomeroy <pomeroyc@outlook.com>
2022-08-17 16:15:02 -0400

    calendar fixes

--- src/pages/eventDetails/[id].js
+++ src/pages/eventDetails/[id].js
@@ -7,7 +7,6 @@ import Seo from "../../components/seo"
 //MUI
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
 import { green } from '@mui/material/colors';
 import { DownloadRounded } from "@mui/icons-material";
 import Paper from '@mui/material/Paper';
@@ -15,7 +14,6 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Grid from "@mui/material/Grid";
 import Typography from '@mui/material/Typography';
-import Fab from '@mui/material/Fab';
 import List from '@mui/material/List';
 import ListSubheader from '@mui/material/ListSubheader';
 import { QrCodeScanner } from "@mui/icons-material";
@@ -24,15 +22,6 @@ import VideoLibraryRoundedIcon from '@mui/icons-material/VideoLibraryRounded';
 
 //import the events JSON
 var events = require('../events.json').events;
-
-   const StyledFab = styled(Fab)({
-    position: 'absolute',
-    zIndex: 1,
-    top: -30,
-    left: 0,
-    right: 0,
-    margin: '0 auto',
-  });
 
 
 function SelectedEvent(props) {
