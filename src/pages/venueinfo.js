09bf7514d76ccfd10e7ab06db2d2e8475f65ecdc
chloepomeroy <pomeroyc@outlook.com>
2022-08-18 16:03:45 -0400

    venue

--- src/pages/venueinfo.js
+++ src/pages/venueinfo.js
@@ -2,11 +2,18 @@ import * as React from "react"
 
 //Components
 import Layout from "../components/layout"
+import Map from "../components/Map/map"
+
+const location = {
+    address: '1600 Amphitheatre Parkway, Mountain View, california.',
+    lat: 37.42216,
+    lng: -122.08427,
+  } 
 
 function VenueInfo () {
     return (
         <Layout>
-
+            <Map location={location} zoomLevel={17} />
         </Layout>
     )
 }
