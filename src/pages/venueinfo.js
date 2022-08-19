02341e976c6e2afa499e96b9162b5ae9a9bdbb79
chloepomeroy <pomeroyc@outlook.com>
2022-08-19 15:44:31 -0400

    basic venue page

--- src/pages/venueinfo.js
+++ src/pages/venueinfo.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
 
 //Components
 import Layout from "../components/layout"
@@ -13,7 +14,15 @@ function VenueInfo () {
             <h5>Ottawa Conference Centre, 55 Colonel By Dr, Ottawa, ON</h5>
             <Grid container spacing={2} alignItems="center" justifyContent="center">
             <Grid item xs={12}>
-            <StaticImage src="../images/shawcentre.png" alt="map"/>
+            {/* <StaticImage src="../images/shawcentre.png" alt="map"/> */}
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2800.2629369346737!2d-75.69396168418135!3d45.42420074413206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cce0501028f9623%3A0x9d3495d801b96afe!2sShaw%20Centre!5e0!3m2!1sen!2sca!4v1660937763316!5m2!1sen!2sca"
+                width="600"
+                height="450"
+                style={{ border: "0" }}
+                allowfullscreen=""
+                loading="lazy"
+            ></iframe>
             </Grid>
             <Grid item xs={12}>
             <Button variant="contained" color="success" href="https://www.shaw-centre.com/attendee/location/">Parking Information</Button>
