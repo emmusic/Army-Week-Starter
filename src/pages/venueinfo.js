56e37409c4485451e953b07a478b89cade187edd
chloepomeroy <pomeroyc@outlook.com>
2022-08-19 15:24:58 -0400

    placeholder picture and link to venue website

--- src/pages/venueinfo.js
+++ src/pages/venueinfo.js
@@ -1,19 +1,24 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 //Components
 import Layout from "../components/layout"
-import Map from "../components/Map/map"
-
-const location = {
-    address: '1600 Amphitheatre Parkway, Mountain View, california.',
-    lat: 37.42216,
-    lng: -122.08427,
-  } 
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 
 function VenueInfo () {
     return (
         <Layout>
-            <Map location={location} zoomLevel={17} />
+            <h2>Venue Info</h2>
+            <h5>Ottawa Conference Centre, 55 Colonel By Dr, Ottawa, ON</h5>
+            <Grid container spacing={2} alignItems="center" justifyContent="center">
+            <Grid item xs={12}>
+            <StaticImage src="../images/shawcentre.png" alt="map"/>
+            </Grid>
+            <Grid item xs={12}>
+            <Button variant="contained" color="success" href="https://www.shaw-centre.com/attendee/location/">Parking Information</Button>
+            </Grid>
+            </Grid>
         </Layout>
     )
 }
