edcf6f83ea9d0da0f877be5e06679722a49b3179
chloepomeroy <pomeroyc@outlook.com>
2022-08-25 12:40:52 -0400

    events fully translated and adding faq

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -19,44 +19,41 @@ let strings = new LocalizedStrings({
         title: "FAQ",
         rows: [
             {
-                title: "How do I register for an event?",
-                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat,
-                  ultricies metus at, consequat velit. Curabitur est nibh, varius in tellus nec, mattis pulvinar metus.
-                  In maximus cursus lorem, nec laoreet velit eleifend vel. Ut aliquet mauris tortor, sed egestas libero interdum vitae.
-                  Fusce sed commodo purus, at tempus turpis.`,
+                title: "Where can I find the presentation?",
+                content: `From the calendar you can click on a particular event which will show you further information about that event, 
+                including the presentation.`,
             },
             {
-                title: "Where can I see an event's location?",
+                title: "Do I need to register?",
                 content:
-                    "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
+                    `Yes, you need to register for the Zoom Webinar for each part of army week, regardless if you are participating virtually 
+                    or in-person. You would have received an Outlook calendar invite for each part of Army Week that included a link to 
+                    register for the Zoom Webinar.`,
             },
             {
-                title: "How can I contact the event organizer",
-                content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
-                Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
-                Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
-                Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
+                title: "How do I check in to an event?",
+                content: `Scan the QR Code on your phone and you'll be redirected to a page that will ask you to type your name.`,
             },
             {
-                title: "Are COVID Protocols in place?",
-                content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
-                Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
-                Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
-                Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. ` ,
+                title: "Where can I see the event's location?",
+                content: `From the calendar you can click on a particular event which will show you further information about that event, 
+                including the location.` ,
             },
             {
-                title: "Do events have limited capacity?",
-                content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
-                Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
-                Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
-                Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. ` ,
+                title: "How can I contact the event's organizer?",
+                content: `If you have questions not answered by this FAQ yoou can contact the organizers at (email address).` ,
             },
             {
-                title: "How do I 'Check in' to an event?",
-                content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
-                Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
-                Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
-                Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. ` ,
+                title: "What Covid protocols are in place?",
+                content: ` Non-Medical Masks are required indoors throughout Army Week, except by speakers and panelists, and when eating or drinking.` ,
+            },
+            {
+                title: "Do in-person events have limited capacity?",
+                content: `Yes. Participation in-person is limited to those invited to participate in person as per the Army Week 2022 Operation Order.` ,
+            },
+            {
+                title: "Are all events open to anyone?",
+                content: `No. Participation is limited to those invited as per the Army Week 2022 Operation Order. ` ,
             },
         ],
     },
@@ -67,14 +64,14 @@ let strings = new LocalizedStrings({
         title: "FAQ",
         rows: [
             {
-                title: "Comment est-ce que je m'inscris pour un évènement?",
+                title: "Ou est-ce-que je peux trouver le présentation?",
                 content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat,
                   ultricies metus at, consequat velit. Curabitur est nibh, varius in tellus nec, mattis pulvinar metus.
                   In maximus cursus lorem, nec laoreet velit eleifend vel. Ut aliquet mauris tortor, sed egestas libero interdum vitae.
                   Fusce sed commodo purus, at tempus turpis.`,
             },
             {
-                title: "Where can I see an event's location?",
+                title: "Est-ce que j'ai besion de m'inscrire?",
                 content:
                     "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
             },
